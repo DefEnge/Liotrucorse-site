9fd096ee81911c676cfbbb114f17a5aa8b855c69
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-04-19 10:20:13 +0200

    readded browsertype router

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
-import {createHashRouter, RouterProvider} from "react-router-dom" ;
+import {createBrowserRouter, RouterProvider} from "react-router-dom" ;
 
 import { Page_Elettronica, Page_Meccanica, Page_Informatica, Page_Management } from './components';
 
 
 import './index.css'
 
-const router = createHashRouter([
+const router = createbrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "DivisioneElettronica",
-        element: <Page_Elettronica />,
-      },
-    ],
+    element: <App/> ,
   },
+
+  {
+    path: "/Divisione_Elettronica",
+    element: <Page_Elettronica/>,
+  },
+
+  {
+    path: "Divisione_Meccanica",
+    element: <Page_Meccanica/>,
+  },
+
+  {
+    path: "Divisione_Informatica",
+    element: <Page_Informatica/>,
+
+  },
+  {
+    path: "Divisione_Management",
+    element: <Page_Management/>,
+  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
