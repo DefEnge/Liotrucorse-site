4ee68d0c4b832e85e3e1273bb423deae60a31516
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-04-19 11:27:34 +0200

    crying in hashroute

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
-import {createBrowserRouter, RouterProvider} from "react-router-dom" ;
+import {createHashRouter, RouterProvider} from "react-router-dom" ;
 
 import { Page_Elettronica, Page_Meccanica, Page_Informatica, Page_Management } from './components';
 
 
 import './index.css'
 
-const router = createbrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element: <App/> ,
+    element: <App/>,
+    children: [
+      {
+        path: "#/DivisioneElettronica",
+        element: <Page_Elettronica />,
+      },
+    ],
   },
-
-  {
-    path: "/Divisione_Elettronica",
-    element: <Page_Elettronica/>,
-  },
-
-  {
-    path: "Divisione_Meccanica",
-    element: <Page_Meccanica/>,
-  },
-
-  {
-    path: "Divisione_Informatica",
-    element: <Page_Informatica/>,
-
-  },
-  {
-    path: "Divisione_Management",
-    element: <Page_Management/>,
-  },
-
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
