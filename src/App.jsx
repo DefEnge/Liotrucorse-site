053669b4841e37e5373afefaefa1f5fbb479225e
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-04-20 12:56:42 +0200

    Finally fixed the routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,8 @@ const App = () => (
       <Route exact path ="/" Component={Main}></Route>
       <Route exact path ="/DivisioneElettronica" Component={Page_Elettronica}></Route>
       <Route exact path ="/DivisioneInformatica" Component={Page_Informatica}></Route>
-      <Route exact path ="/DivisionMeccanica" Component={Page_Meccanica}></Route>
+      <Route exact path ="/DivisioneMeccanica" Component={Page_Meccanica}></Route>
       <Route exact path ="/DivisioneManagement" Component={Page_Management}></Route>
-
-      
-
-      
-
   </Routes>
   </HashRouter>
 )
