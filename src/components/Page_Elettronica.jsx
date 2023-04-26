fe4e1e0ee1ae0f01bfe72e1ab8fa0d5b293e615b
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-04-26 16:40:46 +0200

    added import

--- src/components/Page_Elettronica.jsx
+++ src/components/Page_Elettronica.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import styles from '../style'
 
 const Page_Elettronica = () => (
   <div className="bg-withe w-full overflow-hidden">
--- src/components/Page_Management.jsx
+++ src/components/Page_Management.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
+import styles from '../style'
 
 
 const Page_Management = () => (
   <div className="bg-withe w-full overflow-hidden">
-  <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-    <div className={`${styles.boxWidth}`}>
-      <Navbar></Navbar>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar></Navbar>
+      </div>
     </div>
   </div>
-</div>
   )
 
 export default Page_Management
