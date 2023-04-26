529465c6b8c270aa487123079300ccfaadf40fc8
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-04-26 16:37:04 +0200

    modified navbar on division pages

--- src/components/Page_Elettronica.jsx
+++ src/components/Page_Elettronica.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Page_Elettronica = () => (
-    <div>
+  <div className="bg-withe w-full overflow-hidden">
+  <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.boxWidth}`}>
       <Navbar></Navbar>
-
     </div>
+  </div>
+</div>
   )
 
 
--- src/components/Page_Informatica.jsx
+++ src/components/Page_Informatica.jsx
@@ -3,9 +3,13 @@ import Navbar from './Navbar'
 
 
 const Page_Informatica = () => (
-    <div>
+  <div className="bg-withe w-full overflow-hidden">
+  <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.boxWidth}`}>
       <Navbar></Navbar>
     </div>
+  </div>
+</div>
   )
 
 export default Page_Informatica
