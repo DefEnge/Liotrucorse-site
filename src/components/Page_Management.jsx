6858a1f6e5d4391f32d81365146d68bed76f6bb8
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-05-08 23:57:20 +0200

    added footer to all pages

--- src/components/Page_Management.jsx
+++ src/components/Page_Management.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
 import styles from '../style'
+import Footer from './Footer'
 
 
 const Page_Management = () => (
-  <div className="bg-withe w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar></Navbar>
-      </div>
+  <div className="bg-white w-full overflow-hidden">
+        
+  <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Navbar></Navbar>
     </div>
   </div>
+
+  <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Footer></Footer>
+    </div>
+  </div>
+
+
+</div>       
   )
 
 export default Page_Management
--- src/components/Page_Meccanica.jsx
+++ src/components/Page_Meccanica.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import styles from '../style'
-
+import Footer from './Footer'
 import Navbar from './Navbar'
 
 const Page_Meccanica = () =>  (
-  <div className="bg-withe w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar></Navbar>
-      </div>
-    </div>
-  </div>
+  <div className="bg-white w-full overflow-hidden">
+        
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar></Navbar>
+          </div>
+        </div>
+      
+        <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Footer></Footer>
+          </div>
+        </div>
+
+
+      </div>       
   
   )
 
