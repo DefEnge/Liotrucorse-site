3185b1c0a6e2e9a7a69089a8b8d5f37e7cf53fae
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-10-18 17:47:31 +0200

    chore:modified main and footer to standby

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ import Footer from './Footer';
 import Contactmain from './Contactmain';
 import ObjectiveOverview from './ObjectiveOverview';
 
-
+/*
 
 const Main = () => (
   <div className="bg-white w-full overflow-hidden">
@@ -52,6 +52,26 @@ const Main = () => (
 
 
   </div>
+)*/
+
+const Main = () => (
+  <div className="bg-white w-full overflow-hidden">
+
+    <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar></Navbar>
+      </div>
+    </div>
+
+    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Footer></Footer>
+      </div>
+    </div>
+
+
+  </div>
 )
+
 
 export default Main
